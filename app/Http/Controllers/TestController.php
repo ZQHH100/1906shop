6000bff75fad1e51c021658b7c733fda92c8e41b
zqhh <sc164391165@qq.com>
2020-03-11 12:31:43 +0000

    mysql测试

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
-
+use Illuminate\Support\Facades\DB;
 class TestController extends Controller
 {
     public function redis1(){
@@ -17,5 +17,9 @@ class TestController extends Controller
         $key = 'aaaa';
         $val = Redis::get($key);
         echo "val:". $val;
+    }
+    public function mysql1(){
+        $list = DB::table('p_users')->first();
+        var_dump($list);
     }
 }
